36a130ec804d423d29ce87f53413f0c17617f68d
Camlin <camlin@carolina.rr.com>
2015-07-03 17:32:31 -0400

    changes to cli.ex

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -28,22 +28,15 @@ defmodule Issues.CLI do
 	"""
 	
 	def parse_args(argv) do
-		parse = OptionParser.parse(argv, switches: [help: :boolean],
-										 aliases:  [ h:   :help   ])
+		parse = OptionParser.parse(argv, switches: [ help: :boolean ],
+										 aliases:  [ h:    :help    ])
 		
 		case parse do
 		
-		{ [ help: true], _, _ }
-		  -> :help
-		
-		{ _, [ user, project, count ], _ }
-		  -> { user, project, String.to_integer(count) }
-		
-		{ _, [ user, project ], _}
-		  -> { user, project, @default_count }
-		
-		_ -> :help
-		
+		{ [ help: true], _,            _ } -> :help
+		{ _, [ user, project, count ], _ } -> { user, project, String.to_integer(count) }
+		{ _, [ user, project ],        _ } -> { user, project, @default_count }
+		_                                  -> :help
 		end
 	end
 	
