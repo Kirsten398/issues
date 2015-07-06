0abc6c011e1b577be3e29a0ecaff8b48e9036314
Camlin <camlin@carolina.rr.com>
2015-07-06 18:30:33 -0400

    more part 4

--- mix.exs
+++ mix.exs
@@ -2,11 +2,13 @@ defmodule Issues.Mixfile do
 	use Mix.Project
 
 	def project do
-		[app:    :issues,
-		version: "0.0.1",
-		elixir:  "~> 1.0",
-		escript: escript_config,
-		deps:    deps]
+		[ app:       :issues,
+		  version:    "0.0.1",
+		  name:       "Issues",
+		  source_url: "https://github.com/pragdave/issues",
+		  elixir:     "~> 1.0",
+		  escript:    escript_config,
+		  deps:       deps ]
 	end
 
 	# Configuration for the OTP application
@@ -16,8 +18,9 @@ defmodule Issues.Mixfile do
 
 	def deps do
 		[
-			{ :httpoison, "~> 0.7.0" },
-			{ :jsx,       "~> 2.0"   }
+			{ :httpoison, "~> 0.7.0"                   },
+			{ :jsx,       "~> 2.0"                     },
+			{ :ex_doc,    github: "elixir-lang/ex_doc" }
 		]
 	end
 	
